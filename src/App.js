96f0b39bfb484186fcc479b5e2a78178c7367031
Dave O <david.olivier@tech.geocent.com>
2020-05-20 16:50:20 -0500

    ultra-minimal proof of concept

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import easter_egg from './easter_egg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={easter_egg} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Congratulations, you have found the magic egg.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Now you should <a className="App-link" href="https://www.geocent.com/careers/openings/" target="_blank" rel="noopener noreferrer" >apply for a job at Geocent</a>.
+        </p>
       </header>
     </div>
   );
